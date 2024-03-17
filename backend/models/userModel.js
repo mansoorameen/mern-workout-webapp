b15704eda1df8843e4e057f62073ee124d5f827c
mansoor <manzooravail@gmail.com>
2024-03-17 18:04:51 +0530

    add: backend signing up and password hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,22 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  // salts adds extra random characters to the password for extra safety
+  // if two passwords are same, as salt is different the final hash will also be different
+  // so if one password is cracked, the other is safe
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
