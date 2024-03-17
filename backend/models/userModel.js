abf896ed8596554e3a8e666486d8ba4ec27a8349
mansoor <manzooravail@gmail.com>
2024-03-17 18:33:08 +0530

    add: email and password validations

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -16,11 +17,21 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  } else if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw Error("Enter a strong password");
+  }
+
+  console.log("first");
   const exists = await this.findOne({ email });
   if (exists) {
     throw Error("Email already in use");
   }
-
+  console.log("sec");
   // salts adds extra random characters to the password for extra safety
   // if two passwords are same, as salt is different the final hash will also be different
   // so if one password is cracked, the other is safe
